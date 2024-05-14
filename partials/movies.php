0e62bdb2b13cdc009b859ec103b927859f7e7600
sofiacottone <cottonesofia@outlook.it>
2024-05-14 17:58:48 +0200

    feat: add minor style detail

--- partials/movies.php
+++ partials/movies.php
@@ -2,9 +2,9 @@
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3">
         <?php foreach ($movies as $movie) { ?>
             <div class="col">
-                <div class="card">
+                <div class="card border-info mb-3">
+                    <div class="card-header fw-bold"><?php echo $movie->title; ?></div>
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $movie->title; ?></h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary"><?php echo $movie->year . ' - ' . $movie->duration;; ?></h6>
                         <div class="card-text">Directed by: <?php echo $movie->director; ?></div>
                         <div class="card-text">Original Language: <?php echo $movie->original_language; ?></div>
